29c6650b406b4607f0919e5bcc405c31bde47588
higorito <higorps198@gmail.com>
2021-08-14 16:21:57 -0300

    colocando colisão no personagem (alguns problemas na velocidade)

--- src/jogo/Controle.java
+++ src/jogo/Controle.java
@@ -1,5 +1,14 @@
 package jogo;
 
-public class Controle {
+import jplay.GameObject;
+import jplay.TileInfo;
 
+public class Controle {
+	
+	public boolean colisao(GameObject obj, TileInfo tile){
+		if((tile.id>=13) && obj.collided(tile)) {   //esse 7 depende da lista la no .scn
+			return true;
+		}
+		return false;
+	}
 }
--- src/jogo/Fase1.java
+++ src/jogo/Fase1.java
@@ -1,6 +1,7 @@
 package jogo;
 
 import jplay.Scene;
+import jplay.Keyboard;
 import jplay.URL;
 import jplay.Window;
 
@@ -11,21 +12,33 @@ public class Fase1 {
 	
 	private Jogador jogador;
 	
+	private Keyboard teclado;
+	
 	public Fase1(Window window) {
 		janela = window;
 		
 		cena = new Scene();
 		cena .loadFromFile(URL.scenario("Cenario_Fase1.scn"));
 		
-		jogador = new Jogador(640,350);
+		jogador = new Jogador(540,350);
+		teclado = janela.getKeyboard();
+		
+		Som.play("musicmine.wav");
 		run();
 	}
 	private void run() {
 		while(true) {
-			 cena.draw();
-			 jogador.draw();
-			 jogador.mover(janela);
+			 jogador.mover(janela, teclado);
+			jogador.caminho(cena);
+			 
+			 cena.moveScene(jogador);  //centralizar o jogador no cenario //tipo camera
+			 
+			 jogador.x += cena.getXOffset();        //p/ movimentação ser mais suave
+			 jogador.y += cena.getYOffset();			//meio que movimentar junto com o cenario
+			 
+			 jogador.draw();			 
 			 janela.update();
+			
 		}
 	}
 }
